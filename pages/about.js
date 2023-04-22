840b0479629b86cdd382f4a25fb70569d736fdf3
yj-toyama <yuji.toyama@gmail.com>
2023-04-22 11:25:41 +0900

    first

---
+++ pages/about.js
@@ -0,0 +1,39 @@
+import styles from 'styles/index.module.css'
+import Hero from 'components/hero'
+import Container from 'components/container'
+import PostBody from 'components/post-body'
+import Contact from 'components/contact'
+import { TwoColumn,TwoColumnMain,TwoColumnSidebar } from 'components/two-column'
+
+export default function About() {
+  return ( 
+  <Container>
+  <TwoColumn>
+
+  <TwoColumnMain>
+  <PostBody>
+  <Hero  title="About" subtitle="About Development Activity" />
+  <p>
+    Our Company has great knowledge about IT Architect
+  </p>
+  <h2> Our Policy</h2>
+  <p>
+    Know best , Do best, Just do it.
+  </p>
+  <h3>
+    IT Architect is great job
+  </h3>
+  <p> 
+    Desing best, Do best, Just do it.
+  </p>
+  </PostBody>
+  </TwoColumnMain>
+
+  <TwoColumnSidebar>
+  <Contact />
+  </TwoColumnSidebar>
+  
+  </TwoColumn>
+  </Container>
+  )
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,11 @@
+import Hero from 'components/hero'
+import Container from 'components/container'
+
+
+export default function Blog() {
+  return (
+  <Container>
+  <Hero title="Blog" subtitle="Resent post" />
+  </Container>
+  )
+}
